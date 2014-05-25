5fb21b2182600872afec3d332808c0241db7119c
oschleidt <oliverschleidt@gmail.com>
2014-05-26 00:42:02 +0200

    Use Price class instead of integer

--- Movie.java
+++ Movie.java
@@ -3,20 +3,36 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
+    private Price price;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
         setPriceCode(newpriceCode);
     }
+    
     public int getPriceCode() {
-        return priceCode;
+    	return price.getPriceCode()
     }
+    
     public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+    	switch(priceCode){
+    	case REGULAR:
+    		price = new RegularPrice();
+    		break;
+    	case CHILDRENS:
+    		price = new ChildrensPrice();
+    		break;
+    	case NEW_RELEASE:
+    		price = new NewReleasePrice();
+    		break;
+    	default:
+    		throw new IllegalArgumentException("Incorrect Price Code");
+    	}
     }
+
     public String getTitle (){
         return title;
     }
+    
     double getCharge(int daysRented) {
     	double result = 0;
     	switch (getPriceCode()) {
