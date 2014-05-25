aed1674e095b75fcbc43efe4dbea8ab7b7f4551e
oschleidt <oliverschleidt@gmail.com>
2014-05-26 00:08:02 +0200

    Self Encapsulate Field Setter for priceCode

--- Movie.java
+++ Movie.java
@@ -6,13 +6,13 @@ public class Movie {
     private int priceCode;
     public Movie(String newtitle, int newpriceCode) {
         title = newtitle;
-        priceCode = newpriceCode;
+        setPriceCode(newpriceCode);
     }
     public int getPriceCode() {
         return priceCode;
     }
-    public void setPriceCode(int arg) {
-        priceCode = arg;
+    public void setPriceCode(int priceCode) {
+        this.priceCode = priceCode;
     }
     public String getTitle (){
         return title;
